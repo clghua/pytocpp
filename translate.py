106c9734ec98ed82d800de1a1f58953992cc960f
Calvin Hua <clghua@uwaterloo.ca>
2015-02-01 01:44:46 -0500

    Even more adjustments with translate; {} in bodies are now seperate

--- translate.py
+++ translate.py
@@ -1,5 +1,6 @@
 tabNum = 0 #This number indicates the number of tabs used for the program
 
+#Call this function to write the C++ program
 def mergeAndTranslate(a1, a2, a3):
     return translate(a1 + a2 + a3)
 
@@ -12,16 +13,16 @@ def translate(a1):
         if(type(a1[i]).__name__ == "str"): #Adding strings... obviously
             c += a1[i]
             #Checking for body checks
-            if '{' in a1[i] or '}' in a1[i] or ';' in a1[i]: #Checking for end line to add tabs
-                if '{' in a1[i]:
+            if "{" == a1[i] or "}" == a1[i] or ';' in a1[i]: #Checking for end line to add tabs
+                if "{" == a1[i]:
                     c += "\n"
                     tabNum += 1
-                elif '}' in a1[i]:
-                    #if tabNum > 0:
-                        #c = c[:-2] + c[-1] #Deleting second last character in string (which should be a tab)
+                elif "}" == a1[i]:
+                    if tabNum > 0:
+                        c = c[:-2] + c[-1] #Deleting second last character in string (which should be a tab)
                     c += "\n"
                     tabNum -= 1
-                for i in range(tabNum):
+                for j in range(tabNum):
                     c += "\t"
         elif type(a1[i]).__name__ == "float" or type(a1[i]).__name__ == "int": #Joining numbers
             c += str(a1[i])
@@ -33,5 +34,5 @@ def translate(a1):
 #This is an example, please change accordingly
 a = ["#include <iostream>\n", "using namespace std;\n"]
 b = []
-c = ["int main () {", "int a", ";\n", "a", " = ", [2, " * ", 5.6], " + ", 4, ";\n","return 0;\n" + "}"]
+c = ["int main ()\n", "{", "int a", ";\n", "a", " = ", [2, " * ", 5.6], " + ", 4, ";\n","return 0;\n", "}"]
 print (mergeAndTranslate(a, b, c))
